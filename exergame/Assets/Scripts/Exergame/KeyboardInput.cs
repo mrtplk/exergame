34960a937cf345617558f62ff498c3ef510f74a0
mrtplk <martapalka13@gmail.com>
2021-12-03 10:52:36 +0100

    Smooth player movement

--- exergame/Assets/Scripts/Exergame/KeyboardInput.cs
+++ exergame/Assets/Scripts/Exergame/KeyboardInput.cs
@@ -4,39 +4,12 @@ using UnityEngine;
 
 public class KeyboardInput : MonoBehaviour
 {
-    public Vector3 direction;
-    public float speed = 30.0f;
-
-    void Start()
-    {
-        direction = new Vector3(0.0f, 0.0f, 0.0f);
-    }
+    public float speed = 5.0f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            direction = Vector3.forward;
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            direction = Vector3.back;
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            direction = Vector3.right;
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            direction = Vector3.left;
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
-
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
     }
 }
